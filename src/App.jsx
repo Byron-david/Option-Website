aefa0b025e2d90fc787c04cb3626d84d4cd5e4aa
Byron David <byrondavid87@gmail.com>
2024-06-01 07:07:20 -0700

    -Added color themes -Added basic outline.txt -Added label/input

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,77 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function CustomInput( {width="50px", padding="0.5rem"}) {
+  const inputStyle = {
+    width: width,
+  }
+  const labelStyle = {
+    padding: padding,
+  }
+
+  // Controls state of input
+  const [value, setValue] = useState("");
+
+  return (
+    <>
+      <label for="symbolName" style={labelStyle}>Symbol Name:</label>
+      <input
+        style={inputStyle}
+        id="symbolName"
+        type="text"
+        placeholder="AAPL"
+        name="symbol"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
+    </>
+
+  );
+}
+
+function Button({ text, color, fontSize, margin }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px",
+    margin: margin + "rem"
+  };
+
+  return <button style={buttonStyle}>{text}</button>;
+}
+
+Button.defaultProps = {
+  text: "Click Me!",
+  color: "blue",
+  fontSize: 12,
+  margin: 0.5
+};
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Add Trade</h1>
+      <div id="addTrade">
+      {/* -Symbol
+      -Strike Price
+      -Exp. Date
+      -Premium Collected
+      -Spreads?
+      -Quantity
+      -Date executed
+      -Open or Close? */}
+        <CustomInput />
+         <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
