76350e6972617b60520a36dcb05f5dad15a0b0b7
Byron David <byrondavid87@gmail.com>
2024-06-05 10:54:44 -0700

    -Added conditionals for stock or option selection

--- src/components/StrategiesDropdown.jsx
+++ src/components/StrategiesDropdown.jsx
@@ -3,32 +3,56 @@ import TextInput from './TextInput.jsx'
 import DropdownInput from './DropdownInput.jsx'
 import { v4 as uuidv4 } from 'uuid';
 
+// function Dropdown( { props, strategyNames, strategyValue, handleChange } ) {
+//   return (
+//     <DropdownInput 
+//       {...props} 
+//       value={strategyValue} 
+//       items={strategyNames} 
+//       onChange={handleChange} 
+//       htmlFor="strategyInput" 
+//       name="strategyInput" 
+//       id="strategyInput" 
+//       text="Strategy:" />
+//   )
+// }
+
 function StrategiesDropDown(props) {
   const strategyNames = [
     { id: uuidv4(), value: "stock", text: "Stock" },
     { id: uuidv4(), value: "singleOption", text: "Option" },
-  ]
-
-  const [strategyValue, setStrategyValue] = useState("Stock");
+  ];
+  const [strategyValue, setStrategyValue] = useState("stock");
 
   function handleChange(e) {
     setStrategyValue(e.target.value);
   }
 
-  // if (strategyValue === "stock") {
-  //     return <DropdownInput {...props} items={strategyNames} textValue={strategyValue} onChange={handleChange} htmlFor="strategyInput" name="strategyInput" id="strategyInput" text="Strategy:" />
-  // }
+  const dropDown = <DropdownInput 
+                      {...props} 
+                      value={strategyValue} 
+                      items={strategyNames} 
+                      onChange={handleChange} 
+                      htmlFor="strategyInput" 
+                      name="strategyInput" 
+                      id="strategyInput" 
+                      text="Strategy:" />
 
-  // if (strategyValue === "ironCondor" || strategyValue === "butterfly") {
-  //     return <DropdownInput {...props} items={strategyNames} textValue={strategyValue} onChange={handleChange} htmlFor="strategyInput" name="strategyInput" id="strategyInput" text="Strategy:" />
-  // }
 
-  return (
-    <>
-      <DropdownInput {...props} items={strategyNames} onChange={handleChange} htmlFor="strategyInput" name="strategyInput" id="strategyInput" text="Strategy:" />
-      <TextInput placeholder="50" inputType="number" id="strikePrice" name="strikePrice" htmlFor="strikePrice" text="Strike Price:" />
-    </>
-  );
+
+
+  if (strategyValue === "singleOption") {
+    return (
+      <>
+        {dropDown}
+        <TextInput placeholder="50" inputType="number" id="strikePrice" name="strikePrice" htmlFor="strikePrice" text="Strike Price:" />
+        
+      </>
+    );
+  }
+  else {
+    return dropDown
+  }
 }
 
 export default StrategiesDropDown
