69b244d9356ad5819a365e88cfea4babf274b442
Byron David <byrondavid87@gmail.com>
2024-06-08 07:07:39 -0700

    Trying to use nextId instead of index for Add/remove trade

--- src/components/AddTrade.jsx
+++ src/components/AddTrade.jsx
@@ -4,15 +4,7 @@ import DateInput from './DateInput.jsx'
 import Button from './Button.jsx'
 import AddOption from './AddOption.jsx'
 import StrategiesDropdown from './StrategiesDropdown.jsx'
-
-function AddOptionList(props) {
-  if (props.items.length === 0) {
-    return null;
-  }
-  return (
-    props.items.map((item, index) => ( <AddOption key={index} item={item}/> ))
-   );
-}
+import { v4 as uuidv4 } from 'uuid';
 
 let nextId = 0;
 
@@ -34,26 +26,39 @@ function AddTrade({ display = "flex", flexDirection = "row", flexWrap = "nowrap"
 
   const handleButtonClickAdd = () => {
     console.log("Adding....");
-    // let newArray = 
-    // setArtists([
-    //   ...artists, { id: nextId++, name: name } 
-    // ]);
-    let strike = inputNames.strikePrice = "strikePrice" + nextId;
-    let value = inputNames.optionValue = "optionValue" + nextId;
-    let quantity = inputNames.quantity = "quantity" + nextId;
-    let exp = inputNames.exp = "exp" + nextId;
-
     setAddOption([
       ...addOption,
-      { id: nextId++, strikePrice: strike, optionValue: value, quantity: quantity, exp: exp }
+      { id: nextId++,
+        strikePrice: "strikePrice" + nextId, 
+        optionValue: "optionValue" + nextId, 
+        quantity: "quantity" + nextId, 
+        exp: "exp" + nextId }
     ]);
   };
 
   const handleButtonClickRemove = () => {
     console.log("Removing....")
+    setAddOption([
+      ...addOption,
+      { id: nextId--,
+        strikePrice: "strikePrice" + nextId, 
+        optionValue: "optionValue" + nextId, 
+        quantity: "quantity" + nextId, 
+        exp: "exp" + nextId }
+    ]);
     // setAddOption(addOption - 1);
-    console.log(addOption);
-  };
+    // {artists.map(artist => (
+    //   <li key={artist.id}>
+    //     {artist.name}{' '}
+    //     <button onClick={() => {
+    //       setArtists(
+    //         artists.filter(a =>
+    //           a.id !== artist.id
+    //         )
+    //       );
+    //     }}>
+    // };
+    };
 
   return (
     <>
@@ -62,15 +67,11 @@ function AddTrade({ display = "flex", flexDirection = "row", flexWrap = "nowrap"
         <div id="addTradeBody">
           <TextInput placeholder="AAPL" maxLength="4" id="stockSymbol" name="stockSymbol" htmlFor="stockSymbol" text="Symbol Name:" />
           <StrategiesDropdown htmlFor="strategyInput" name="strategyInput" id="strategyInput" text="Strategy:" />
-          <DateInput id="dateExec" name="dateExec" htmlFor="dateExec" text=" Date Exec." />
+          <DateInput  id="dateExec" name="dateExec" htmlFor="dateExec" text=" Date Exec." />
+          <TextInput placeholder="1" type="number" id="quantityNumber" name="quantityNumber" htmlFor="quantityNumber" text="Qty:" />
           {addOption.map((option, index) => (
           <AddOption key={index} handleClick={handleButtonClickRemove} strikePrice={option.strikePrice} optionValue={option.optionValue} quantity={option.quantity} exp={option.exp} />
           ))}
-          {/* <AddOptionList items={addOption} handleClick={handleButtonClickRemove} strikePrice={} optionValue={} quantity={} exp={}/> */}
-          {/* <TextInput placeholder="50" inputType="number" id="strikePrice" name="strikePrice" htmlFor="strikePrice" text="Strike Price:" /> */}
-          {/* <TextInput placeholder="100" inputType="number" id="stockPrice" name="stockPrice" htmlFor="stockPrice" text="Stock Price:" /> */}
-          {/* <TextInput placeholder="10,000" inputType="number" id="stockValue" name="stockValue" htmlFor="stockValue" text="Value:" /> */}
-          {/* <TextInput placeholder="1" id="quantityValue" inputType="number" iname="quantityValue" htmlFor="quantityValue" text="Qty:" /> */}
         </div>
         <div id="addTradeFooter">
           <Button text="+ Add Option" className="buttonAdd" handleClick={handleButtonClickAdd} />
