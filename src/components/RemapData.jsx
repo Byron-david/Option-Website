d3ea8853d3158d3771fcb749d54048bd0ead848c
Byron David <byrondavid87@gmail.com>
2024-06-12 17:03:05 -0700

    Remapped reworked. Add in if statements

--- src/components/RemapData.jsx
+++ src/components/RemapData.jsx
@@ -25,7 +25,6 @@ const tastyMapping = {
 const Remap = (data, mapping) => {
   const newArray = [];
   const mappingValues = Object.values(mapping);
-  const mappingKeys = Object.keys(mapping);
 
   for (let i = 0; i < data.length; i++) {
     const obj = {};
@@ -35,55 +34,33 @@ const Remap = (data, mapping) => {
         continue;
     } 
     else {
-      tableHeadNames.forEach((name, index) => (
-        // console.log(dataValues[mappingValues[index]])
-        obj[name] = dataValues[mappingValues[index]]
-      // { [name]: dataValues[mappingValues[index]] }
-      ))
-      newArray.push(obj)
-      // for (const element of tableHeadNames) {
-      //   obj[element] = dataValues[]
-      // const result = dataValues.map((option) => (
-      //   tableHeadNames.forEach((name, index) => (
-      //       // console.log(mappingValues[index])
-      //     { name: [dataValues[mappingKeys[index]]]}
-      //   )
-        
-      //   )))
+      tableHeadNames.forEach((name, index) => {
+        if (name === "Symbol") {
+            let splitValue = value.split(" ")[0];
+            obj[key] = splitValue;
+        }
+        else if (key === "Date") {
+            let date = new Date(value);
+            let day = date.getDay();
+            let month = date.getMonth();
+            let year = date.getFullYear().toString();
+            let dateString = `${day}/${month}/${year.substring(2)}`;
 
+            obj[key] = dateString;
+        }
+        else if (key === "Sub Type") {
+          if (value !== null) {
+            obj[key] = actionValue
+            
+          }
+          else {
+            obj[key] = value
+          }
+        }
+        obj[name] = dataValues[mappingValues[index]]
+    })
+      newArray.push(obj);
       }
-      // else {
-      //     for (const key of mapping) {
-      //         const value = data[i][key];
-      //         if (key === "Symbol") {
-      //             let splitValue = value.split(" ")[0];
-      //             obj[key] = splitValue;
-      //         }
-      //         else if (key === "Date") {
-      //             let date = new Date(value);
-      //             let day = date.getDay();
-      //             let month = date.getMonth();
-      //             let year = date.getFullYear().toString();
-      //             let dateString = `${day}/${month}/${year.substring(2)}`;
-  
-      //             obj[key] = dateString;
-      //         }
-      //         else if (key === "Sub Type") {
-      //           if (value !== null) {
-      //             obj[key] = actionValue
-                  
-      //           }
-      //           else {
-      //             obj[key] = value
-      //           }
-      //         }
-      //         else {
-      //             obj[key] = value
-      //         }
-      //     }
-      //         newArray.push(obj)
-      //     }
-      // newArray["Edit/Remove"] = "";
   }
   console.log(newArray)
   return (
