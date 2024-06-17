9d33c0b3fa98d6dc777745f9b93f1c08e08e46bc
Byron David <byrondavid87@gmail.com>
2024-06-17 15:40:27 -0700

    Added skeleton for Modal. Need to implement it into add trade and add styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import AddTrade from './components/AddTrade.jsx'
 import Navbar from './components/Navbar/Navbar.jsx'
+import Modal from './components/Modal/Modal.jsx'
 // import ImportCSV from './components/ImportCSV.jsx'
 import PositionsTable from './components/PositionsTable/PositionsTable.jsx'
 
 function App() {
-
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <Navbar />
       <div id="mainContainer">
         <main>
+          <button onClick={() => setIsOpen(true)}>Open Modal</button>
+          <Modal open={isOpen} onClose={() => setIsOpen(false)}>I am Open! </Modal>
           <PositionsTable />
-        <AddTrade />
+          <AddTrade />
 
         </main>
       </div>
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import ReactDom from 'react-dom'
+import styles from './Modal.module.css'; 
+
+function Modal({ children, open, onClose}) {
+    
+  if (!open) return null;
+
+  return ReactDom.createPortal(
+    <>
+      {children}
+      <button onClick={onClose}>CLOSE ME</button>
+    </>,
+    document.getElementById('portal')
+  );
+}
+
+export default Modal
