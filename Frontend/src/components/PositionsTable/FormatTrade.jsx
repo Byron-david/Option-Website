d873d6d3c7b6c68716e4d9082f7e392e74100b6e
Byron David <byrondavid87@gmail.com>
2024-08-27 10:15:13 -0700

    -Changed theme to Devias inspired (light)

--- Frontend/src/components/PositionsTable/FormatTrade.jsx
+++ Frontend/src/components/PositionsTable/FormatTrade.jsx
@@ -3,17 +3,7 @@ import styles from './PositionsTable.module.css';
 import TableRow from './TableRow'
 
 export default function FormatTrade({ allTrades, setAllTrades, tableHeader }) {
-
     const strategyNames = allTrades.map(item => Object.keys(item)[0])
-    const keys = tableHeader.map(key =>  { 
-        let lowerKey = key.toLowerCase()
-        if (key === "Exp. Date") {
-          lowerKey = key.split(". ")
-          lowerKey = lowerKey.join("")
-          lowerKey = lowerKey.toLowerCase()
-        }
-        return (lowerKey)
-      })
 
     return (
         <>
