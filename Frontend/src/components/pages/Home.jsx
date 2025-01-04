3eb010ef0357b335cbd8b57e715282b0b925b682
Byron David <byrondavid87@gmail.com>
2025-01-03 21:55:04 -0800

    Added Header and subheader to Home.jsx

--- Frontend/src/components/pages/Home.jsx
+++ Frontend/src/components/pages/Home.jsx
@@ -4,7 +4,8 @@ import styles from './Pages.module.css';
 function Home() {
   return (
     <>
-      <h1 id={styles.homeH1}>Stock and Option Tracking Made Easy</h1>
+      <h1 id={styles.homeH1}>Stock and Option Spread Tracking</h1>
+      <h2 class={styles.homeP}>Track all of your complex spreads simply</h2>
     </>
   )
 }
