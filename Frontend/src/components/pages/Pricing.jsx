f5c64490c5fbc324ce697c480469edc47f42e247
Byron David <byrondavid87@gmail.com>
2025-01-05 08:09:10 -0800

    -Free section of pricing page first pass -Need to create a tag button with bgcolor instead of border

--- Frontend/src/components/pages/Pricing.jsx
+++ Frontend/src/components/pages/Pricing.jsx
@@ -16,9 +16,26 @@ function Pricing() {
               <span> per Month</span>
             </h3>
             <LinkButton to="/signin" text="Join for Free"/>
+            <ul>
+              <li>Single portfolio</li>
+              <li>50 trade strategies</li>
+              <li>Strategy analysis</li>
+            </ul>
           </div>
           <div class={`${styles.textContainer} borderLight`}>
             <h2>Basic</h2>
+            <p class={styles.pMuted}>All you need to be successful</p>
+            <h3 class={styles.header3}>
+              <sup>$</sup>
+              <span class="text30">5</span>
+              <span> per Month</span>
+            </h3>
+            <LinkButton to="/signin" text="Join Now" className="linkBorder"/>
+            <ul>
+              <li>Single portfolio</li>
+              <li>50 trade strategies</li>
+              <li>Strategy analysis</li>
+            </ul>
           </div>
           <div class={`${styles.textContainer} borderLight`}>
             <h2>Pro</h2>
