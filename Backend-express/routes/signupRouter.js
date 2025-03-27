3729aa8b9bba27d7b0b80100dc2d12d439af603b
Byron <byrondavid87@gmail.com>
2025-03-27 13:42:53 -0700

    -Removed signupController and signupRouter to clean files up

--- Backend-express/routes/signupRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const signupController = require("../controllers/signupController");
-const signupRouter = Router();
-
-// signupRouter.get('/users', signupController.getUser)
-signupRouter.post('/', signupController.createUser)
-
-module.exports = signupRouter;
