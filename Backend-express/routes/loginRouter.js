4047d49925c6a5b74080d8eeb44614178ea9a9f6
Byron David <byrondavid87@gmail.com>
2025-12-17 07:25:08 -0800

    Added resetDb route to reset the db

--- Backend-express/routes/loginRouter.js
+++ Backend-express/routes/loginRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const loginRouter = Router();
 const loginController = require("../controllers/loginController");
 const { ensureAuthenticated } = require('../utils/middleware'); // Use your auth middleware
+const { resetDatabase } = require("../db/resetDb");
 
 loginRouter.post('/signup', loginController.createUser)
 loginRouter.post('/login', loginController.loginUser);
@@ -12,5 +13,14 @@ loginRouter.post('/auth', loginController.checkAuth);
 // loginRouter.get('/test-protected', ensureAuthenticated, (req, res) => {
 //     res.json({ message: 'You are authenticated!', user: req.user });
 //   });
-  
+
+loginRouter.get('/reset-db', async (req, res) => {
+    try {
+        await resetDatabase();
+        res.send("Database has been reset!");
+    } catch (error) {
+        res.status(500).send("Error resetting database: " + error.message);
+    }
+});
+
 module.exports = loginRouter;
